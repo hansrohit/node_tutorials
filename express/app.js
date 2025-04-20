a7f8d5531331f85a2507af960d46e78159bcac4e
Hans Rohit <hansrohit031@gmail.com>
2025-04-20 11:33:25 +0530

    second commit

--- express/app.js
+++ express/app.js
@@ -5,10 +5,10 @@ const app = express();
 const port = 3000;
 app.use(express.static("./public"));
 
-app.get("/", (req, res) => {
-  console.log(req);
-  res.status(200).sendFile(path.resolve(__dirname, "./public/index.html"));
-});
+// app.get("/", (req, res) => {
+//   console.log(req);
+//   res.status(200).sendFile(path.resolve(__dirname, "./public/index.html"));
+// });
 
 // app.all("*", (req, res) => {
 //   res.status(404).send("<h1>Page not found</h1>");
