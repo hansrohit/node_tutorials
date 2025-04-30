6d37d564994c923992546737128fb98187a227c0
Hans Rohit <hansrohit031@gmail.com>
2025-04-30 18:44:31 +0530

    middleware-1

--- express/app.js
+++ express/app.js
@@ -17,50 +17,63 @@ const port = 3000;
 //   res.status(404).send("<h1>Page not found</h1>");
 // });
 
-app.get("/", (req, res) => {
-  // res.json([{ name: "hans rohit" }, { name: "jinju" }]);
-  res.send(`<h1>Home</h1></br><a href='/api/students/'>students</a>`);
-  // res.json(students);
-});
+// app.get("/", (req, res) => {
+//   // res.json([{ name: "hans rohit" }, { name: "jinju" }]);
+//   res.send(`<h1>Home</h1></br><a href='/api/students/'>students</a>`);
+//   // res.json(students);
+// });
 
-app.get("/api/students", (req, res) => {
-  const stud = students.map((student) => {
-    const { id, name } = student;
-    return { id, name };
-  });
-  res.json(stud);
-});
+// app.get("/api/students", (req, res) => {
+//   const stud = students.map((student) => {
+//     const { id, name } = student;
+//     return { id, name };
+//   });
+//   res.json(stud);
+// });
 
-app.get("/api/students/:id", (req, res) => {
-  console.log(req);
-  console.log(req.params);
-  const { id } = req.params;
-  const singleStud = students.find((student) => student.id === Number(id));
-  if (!singleStud) return res.status(404).send("<h1>product not found</h1>");
-  res.json(singleStud);
-});
+// app.get("/api/students/:id", (req, res) => {
+//   console.log(req);
+//   console.log(req.params);
+//   const { id } = req.params;
+//   const singleStud = students.find((student) => student.id === Number(id));
+//   if (!singleStud) return res.status(404).send("<h1>product not found</h1>");
+//   res.json(singleStud);
+// });
 
-app.get("/api/sortedStudents/search", (req, res) => {
-  console.log(req.query);
-  const { name, profile, limit } = req.query;
-  let sortedStudents = [...students];
-  if (name) {
-    sortedStudents = sortedStudents.filter((student) => {
-      return student.name.startsWith(name);
-    });
-  }
-  if (profile) {
-    sortedStudents = sortedStudents.filter((student) => {
-      return student.profile.includes(profile);
-    });
-  }
-  if (limit) {
-    sortedStudents = sortedStudents.slice(0, Number(limit));
-  }
-  if (sortedStudents < 1) {
-    res.status(404).send("student not matches");
-  }
-  res.status(200).json(sortedStudents);
+// app.get("/api/sortedStudents/search", (req, res) => {
+//   console.log(req.query);
+//   const { name, profile, limit } = req.query;
+//   let sortedStudents = [...students];
+//   if (name) {
+//     sortedStudents = sortedStudents.filter((student) => {
+//       return student.name.startsWith(name);
+//     });
+//   }
+//   if (profile) {
+//     sortedStudents = sortedStudents.filter((student) => {
+//       return student.profile.includes(profile);
+//     });
+//   }
+//   if (limit) {
+//     sortedStudents = sortedStudents.slice(0, Number(limit));
+//   }
+//   if (sortedStudents < 1) {
+//     res.status(404).send("student not matches");
+//   }
+//   res.status(200).json(sortedStudents);
+// });
+
+const logger = (req, res, next) => {
+  const url = req.url;
+  const method = req.method;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  // res.send("testing");
+  next();
+};
+
+app.get("/", logger, (req, res) => {
+  res.send("home");
 });
 
 app.listen(port, function (err) {
